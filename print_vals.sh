65207eb920ea423e77b09a2f4a3eecbef3046f9c
bmink <barnamink@gmail.com>
2026-05-26 10:11:13 -0700

    Display temps in C as well

--- print_vals.sh
+++ print_vals.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+f_to_c() {
+    awk "BEGIN {printf \"%.1f\", ($1 - 32) * 5/9}"
+}
+
 TEMPF=`redis6-cli get "weather:awn:tempf"`
 TEMPINF=`redis6-cli get "weather:awn:tempinf"`
 HUMID=`redis6-cli get "weather:awn:humid"`
@@ -10,11 +14,17 @@ AGO=$(( NOW - DATE ))
 DAYMINF=`redis6-cli get "weather:awn:dayminf"`
 DAYMAXF=`redis6-cli get "weather:awn:daymaxf"`
 
+TEMPC=$(f_to_c $TEMPF)
+TEMPINC=$(f_to_c $TEMPINF)
+DEWPC=$(f_to_c $DEWP)
+DAYMINC=$(f_to_c $DAYMINF)
+DAYMAXC=$(f_to_c $DAYMAXF)
+
 echo "Weather ($AGO sec ago)"
 echo "-----------------------------"
-echo "Outdoor temp:  $TEMPF °F"
-echo "Indoor temp:   $TEMPINF °F"
+echo "Outdoor temp:  $TEMPF °F ($TEMPC °C)"
+echo "Indoor temp:   $TEMPINF °F ($TEMPINC °C)"
 echo "Humidity:      $HUMID%"
-echo "Dew point:     $DEWP °F"
-echo "Today's low:   $DAYMINF °F"
-echo "Today's high:  $DAYMAXF °F"
+echo "Dew point:     $DEWP °F ($DEWPC °C)"
+echo "Today's low:   $DAYMINF °F ($DAYMINC °C)"
+echo "Today's high:  $DAYMAXF °F ($DAYMAXC °C)"

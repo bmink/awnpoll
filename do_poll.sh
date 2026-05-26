6a451c70b35d8eb9e72f05aecd3daa0a0190a1a4
Barna Mink <barnamink@gmail.com>
2026-05-26 06:51:56 +0000

    Store values in redis. Do daily min/max calculations

--- do_poll.sh
+++ do_poll.sh
@@ -9,22 +9,49 @@ RESP=`curl -s "https://rt.ambientweather.net/v1/devices?applicationKey=$AWN_APP_
 
 #echo "$RESP" | jq . | less
 
-TEMPOUTF=`echo "$RESP" | jq -r '.[0].lastData.tempf'`
+TEMPF=`echo "$RESP" | jq -r '.[0].lastData.tempf'`
 TEMPINF=`echo "$RESP" | jq -r '.[0].lastData.tempinf'`
 HUMID=`echo "$RESP" | jq -r '.[0].lastData.humidity'`
 DEWP=`echo "$RESP" | jq -r '.[0].lastData.dewPoint'`
 DATE=`echo "$RESP" | jq -r '.[0].lastData.dateutc' | sed -e 's/000$//'`
 
-#echo "Tempoutf: $TEMPOUTF"
+#echo "Tempoutf: $TEMPF"
 #echo "Tempinf: $TEMPINF"
 #echo "Humidity: $HUMID"
 #echo "Dew Point: $DEWP"
 #echo "Date: $DATE"
 
-redis6-cli set "weather:awn:tempoutf" "$TEMPOUTF" > /dev/null
+
+redis6-cli set "weather:awn:tempf" "$TEMPF" > /dev/null
 redis6-cli set "weather:awn:tempinf"  "$TEMPINF" > /dev/null
 redis6-cli set "weather:awn:humid"    "$HUMID" > /dev/null
 redis6-cli set "weather:awn:dewp"     "$DEWP" > /dev/null
 redis6-cli set "weather:awn:date"     "$DATE" > /dev/null
+
+
+# Update daily max and min if needed
+CURDAY=`date "+%Y-%m-%d"`
+CURMINMAXDAY=`redis6-cli get "weather:awn:curminmaxday"`
+
+if [ "$CURDAY" != "$CURMINMAXDAY" ]; then
+	# A new day, reset min and max
+
+	redis6-cli set "weather:awn:curminmaxday" "$CURDAY" > /dev/null
+	redis6-cli set "weather:awn:dayminf" "$TEMPF" > /dev/null
+	redis6-cli set "weather:awn:daymaxf" "$TEMPF" > /dev/null
+else
+	# Compare and update as needed
+
+	DAYMINF=`redis6-cli get "weather:awn:dayminf"`
+	if [ "$TEMPF" -lt "$DAYMINF" ]; then
+		redis6-cli set "weather:awn:dayminf" "$TEMPF" > /dev/null
+	fi
+
+	DAYMAXF=`redis6-cli get "weather:awn:daymaxf"`
+	if [ "$TEMPF" -gt "$DAYMAXF" ]; then
+		redis6-cli set "weather:awn:daymaxf" "$TEMPF" > /dev/null
+	fi
+
+fi
 
 
